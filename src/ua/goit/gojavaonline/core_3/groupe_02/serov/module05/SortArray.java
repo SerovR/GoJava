5194c55c318a6a1db458dc4a3d0f9fc46467da72
SerovR <roma.serov.1990@gmail.com>
2016-06-09 14:34:46 +0300

    Array

--- src/ua/goit/gojavaonline/core_3/groupe_02/serov/module05/SortArray.java
+++ src/ua/goit/gojavaonline/core_3/groupe_02/serov/module05/SortArray.java
@@ -7,18 +7,15 @@ public class SortArray {
         for (int i = array.length - 1; i >= 1; i--) {
             for (int index = 0; index < i; index++) {
                 if (array[index] > array[index + 1]) {
-                    swap(array, index);
+                    int tmp = array[index];
+                    array[index] = array[index + 1];
+                    array[index + 1] = tmp;
                 }
             }
         }
         return array;
     }
 
-    public static void swap(int[] array, int k) {
-        int tmp = array[k];
-        array[k] = array[k + 1];
-        array[k + 1] = tmp;
-    }
 }
 
 
