3cdcd02a5023c9e10c155b5bac67f85f6d5df4dc
SerovR <roma.serov.1990@gmail.com>
2016-06-09 22:07:14 +0300

    09/06/16

--- src/ua/goit/gojavaonline/core_3/groupe_02/serov/module05/ArrayUtils.java
+++ src/ua/goit/gojavaonline/core_3/groupe_02/serov/module05/ArrayUtils.java
@@ -1,7 +1,19 @@
 package ua.goit.gojavaonline.core_3.groupe_02.serov.module05;
 
 
-public class SearchMinMax {
+public class ArrayUtils {
+
+    public static void sort(int[] array) {
+        for (int i = array.length - 1; i >= 1; i--) {
+            for (int index = 0; index < i; index++) {
+                if (array[index] > array[index + 1]) {
+                    int tmp = array[index];
+                    array[index] = array[index + 1];
+                    array[index + 1] = tmp;
+                }
+            }
+        }
+    }
     public static int searchMin(int[] array) {
         int min = array[0];
         for (int i = 0; i < array.length; i++) {
@@ -21,4 +33,7 @@ public class SearchMinMax {
         }
         return max;
     }
+
 }
+
+
--- src/ua/goit/gojavaonline/core_3/groupe_02/serov/module05/Run.java
+++ src/ua/goit/gojavaonline/core_3/groupe_02/serov/module05/Run.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 public class Run {
     public static void main(String[] args) {
         int[] array = {2, 3, 8, 6, 34, 23, 13};
-        System.out.println("Min element in array = " + SearchMinMax.searchMin(array));
-        System.out.println("Max element in array = " + SearchMinMax.searchMax(array));
+        System.out.println("Min element in array = " + ArrayUtils.searchMin(array));
+        System.out.println("Max element in array = " + ArrayUtils.searchMax(array));
         System.out.println("Изначальный массив: " + Arrays.toString(array));
-        SortArray.sort(array);
+        ArrayUtils.sort(array);
         System.out.println("Отсортированый массив: " + Arrays.toString(array));
     }
 
